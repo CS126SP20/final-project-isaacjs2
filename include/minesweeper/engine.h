25a41e2acfb16d385dd33c40cc7a5491d82dc38a
isaac <isaacjs2@illinois.edu>
2020-04-21 22:07:13 -0500

    Add game engine files

---
+++ include/minesweeper/engine.h
@@ -0,0 +1,11 @@
+// Copyright (c) 2020 [Your Name]. All rights reserved.
+#ifndef FINALPROJECT_MINESWEEPER_ENGINE_H_
+#define FINALPROJECT_MINESWEEPER_ENGINE_H_
+
+
+namespace minesweeper {
+
+}  // namespace minesweeper
+
+
+#endif // FINALPROJECT_MINESWEEPER_ENGINE_H_
---
+++ src/engine.cc
@@ -0,0 +1,7 @@
+// Copyright (c) 2020 [Your Name]. All rights reserved.
+
+#include <minesweeper/engine.h>
+
+namespace minesweeper {
+
+}  // namespace minesweeper
